4d6077ba5f13e19e1593965ebaf5d3731889df58
datsiuk7 <datcukbogdan@gmail.com>
2026-06-16 15:56:18 +0300

    .

---
+++ flask/site1/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+
+@app.route('/')
+def home():
+    
+    return render_template('index.html', names=names)
+
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+@app.route('/user/<username>')
+def user(username):
+    return f'Hello, {username}!'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'This is post number {post_id}.'
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['myname']
+        names.append(name)
+    return render_template('form.html', names=names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
