612a8f1c6a4bdd86ab080127787a107f1b5cf2a5
datsiuk7 <datcukbogdan@gmail.com>
2026-06-19 19:42:47 +0300

    ,

--- flask/site1/app.py
+++ flask/site1/app.py
@@ -1,11 +1,33 @@
+# pip install Flask-SQLAlchemy
+
+
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Економія пам'яті, відключаємо відстеження змін у базі даних
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+    def __str__(self):
+        return self.username
+
+with app.app_context():
+    db.create_all()
+    
 
 @app.route('/')
 def home():
+    names = User.query.all()
     
     return render_template('index.html', names=names)
 
@@ -26,9 +48,11 @@ def show_post(post_id):
 def form():
     if request.method == 'POST':
         name = request.form['myname']
-        names.append(name)
+        db.session.add(User(username=name))
+        db.session.commit()
         return redirect(url_for('form'))
     
+    names = User.query.all()
     return render_template('form.html', names=names)
 
 if __name__ == '__main__':
